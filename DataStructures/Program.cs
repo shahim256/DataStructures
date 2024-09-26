898978fc2f6bdfde05689970cd7b331f041fd7bd
shahim256 <shahimharis1@gmail.com>
2024-09-26 12:00:35 +0100

    created a class and edited the program

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,15 +1,50 @@
-﻿namespace DataStructures
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace DataStructures
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadKey ();
+            Movie deadpool3;
+            deadpool3.title = "deadpool anbd wolverine";
+            deadpool3.releaseYear = 2024;
+            deadpool3.price = 19.99;
+            deadpool3.rating = MovieRating._15;
+            
+            Console.WriteLine(deadpool3.title + "( " + deadpool3.releaseYear + ") Rated " + deadpool3.rating);
 
-            float somefloat = 3.14159f;
-            Console.WriteLine(somefloat.ToString("0.000"));
             Console.ReadKey();
+            Console.WriteLine("\n\n");
+
+            List<TVShow> shows = new List<TVShow>();
+            shows.Add(new TVShow("The Lord of the Rings: The Rings of Power", 2022, 0, MovieRating._15));
+            shows.Add(new TVShow("The Boys", 2019, 7.99, MovieRating._18));
+            shows.Add(new TVShow("Ben and Holly's Little Kingdom", 2009, 12.45, MovieRating._U));
+
+            for (int i = 0; i < shows.Count; i++)
+            {
+                Console.WriteLine(shows[i].title);
+            }
+            Console.ReadKey();
+
+        }
+
+        public enum MovieRating
+        {
+            _U,
+            _12A,
+            _12,
+            _15,
+            _18,
+        }
+
+        public struct Movie
+        {
+            public string title;
+            public int releaseYear;
+            public double price;
+            public MovieRating rating;
         }
     }
 }
---
+++ DataStructures/TVShow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DataStructures.Program;
+
+namespace DataStructures
+{
+    internal class TVShow
+    {
+        public string title = "Untitled";
+        public int releaseYear;
+        public double price;
+        public MovieRating rating;
+
+        public TVShow(string title, int releaseYear, double price ,MovieRating rating)
+        {
+            this.title = title;
+            this.releaseYear = releaseYear;
+            this.price = price;
+            this.rating = rating;
+        }
+        
+        public override string ToString()
+        {
+            return title + "(" + releaseYear + ") Rated " + rating + " ... £" + price.ToString("0.00");
+        }
+    }
+}
